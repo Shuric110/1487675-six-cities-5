43d89991875109e25fe788fdc477e70e92bfb9eb
Shuric110 <65159520+Shuric110@users.noreply.github.com>
2020-10-21 20:27:13 +0300

    Вынесение формы отзыва в отдельный компонент

---
+++ src/components/review-form/review-form.jsx
@@ -0,0 +1,76 @@
+import React, {PureComponent} from "react";
+import PropTypes from "prop-types";
+
+const StarsLabels = {
+  1: `terribly`,
+  2: `badly`,
+  3: `not bad`,
+  4: `good`,
+  5: `perfect`,
+};
+
+export default class ReviewForm extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      rating: null,
+      text: ``,
+    };
+
+    this.handleRatingChange = this.handleRatingChange.bind(this);
+    this.handleTextChange = this.handleTextChange.bind(this);
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
+  }
+
+  handleRatingChange(evt) {
+    this.setState({rating: +evt.target.value});
+  }
+
+  handleTextChange(evt) {
+    this.setState({text: evt.target.value});
+  }
+
+  handleFormSubmit(evt) {
+    evt.preventDefault();
+    this.props.onFormSubmit(this.state);
+  }
+
+  render() {
+    const {rating, text} = this.state;
+
+    return (
+      <form className="reviews__form form" action="#" method="post" onSubmit={this.handleFormSubmit}>
+        <label className="reviews__label form__label" htmlFor="review">Your review</label>
+        <div className="reviews__rating-form form__rating">
+          {Object.entries(StarsLabels).reverse().map(([stars, title]) => (
+            <React.Fragment key={stars}>
+              <input className="form__rating-input visually-hidden" name="rating"
+                value={stars} id={`${stars}-stars`} type="radio" checked={rating === +stars} onChange={this.handleRatingChange}
+              />
+              <label htmlFor={`${stars}-stars`} className="reviews__rating-label form__rating-label" title={title}>
+                <svg className="form__star-image" width="37" height="33">
+                  <use xlinkHref="#icon-star"></use>
+                </svg>
+              </label>
+            </React.Fragment>
+          ))}
+        </div>
+        <textarea className="reviews__textarea form__textarea" id="review" name="review"
+          placeholder="Tell how was your stay, what you like and what can be improved"
+          onChange={this.handleTextChange} value={text}
+        />
+        <div className="reviews__button-wrapper">
+          <p className="reviews__help">
+            To submit review please make sure to set <span className="reviews__star">rating</span> and describe your stay with at least <b className="reviews__text-amount">50 characters</b>.
+          </p>
+          <button className="reviews__submit form__submit button" type="submit" disabled="">Submit</button>
+        </div>
+      </form>
+    );
+  }
+}
+
+ReviewForm.propTypes = {
+  onFormSubmit: PropTypes.func.isRequired,
+};
